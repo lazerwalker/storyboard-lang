938d2ccafeb0c8da57b35569ef6c295b8a541d5b
Mike Lazer-Walker <michael@lazerwalker.com>
2017-09-30 11:35:32 -0700

    Comments can appear anywhere within a node and are stripped out

--- spec/node.spec.ts
+++ spec/node.spec.ts
@@ -53,4 +53,60 @@ describe("nodes", () => {
 
     })
   })
+
+  describe("comments", () => {
+    context("graph nodes", () => {
+      it("should ignore them", () => {
+        const withComments = `
+          # node
+          -- I am a comment
+          text: Hello!
+          -- Yet another comment
+          -> choice
+          allowRepeats
+          -- I am another comment
+        `
+
+        const withoutComments = `
+          # node
+          text: Hello!
+          -> choice
+          allowRepeats
+        `
+
+        const parsedComments = parseString(withComments)
+        const parsedWithoutComments = parseString(withoutComments)
+
+        expect(parsedComments).to.eql(parsedWithoutComments)
+      })
+    })
+
+    context("bag nodes", () => {
+      it("should ignore them", () => {
+        const withComments = `
+          ## node
+          [predicate]
+          -- I am a comment
+          text: Hello!
+          -- Yet another comment
+          -> choice
+          allowRepeats
+          -- I am another comment
+        `
+
+        const withoutComments = `
+          ## node
+          [predicate]
+          text: Hello!
+          -> choice
+          allowRepeats
+        `
+
+        const parsedComments = parseString(withComments)
+        const parsedWithoutComments = parseString(withoutComments)
+
+        expect(parsedComments).to.eql(parsedWithoutComments)
+      })
+    })
+  })
 })
